ea4ce5fdcd3b1f604e2878d8c0922d05e7ad32ad
Ashley Ellis <ashley@spreedly.com>
2015-08-30 23:49:46 -0400

    Refactor Raindrops

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -5,16 +5,10 @@ class Raindrops
     primes = get_primes(num)
     return num.to_s if primes.empty?
     conversions = {3 => "Pling", 5 => "Plang", 7 => "Plong"}
-    s = []
-    primes.each do |prime|
-      s << conversions[prime]
-    end
-    s.join
+    primes.each_with_object("") { |prime, s| s << conversions[prime] }
   end
 
   def self.get_primes(num)
-    primes = []
-    [3,5,7].each {|prime| primes << prime if num.to_i % prime == 0}
-    primes
+    [3,5,7].each_with_object([]) {|prime, a| a << prime if num.to_i % prime == 0}
   end
 end
