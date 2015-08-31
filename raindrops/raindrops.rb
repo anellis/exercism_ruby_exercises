0782f0c661d63a019967fb48656a549f1c85346e
Ashley Ellis <ashley@spreedly.com>
2015-08-30 23:27:51 -0400

    Raindrops passing

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,14 +1,20 @@
+require 'pry'
 class Raindrops
 
   def self.convert(num)
     primes = get_primes(num)
-    return num.to_is if primes.empty?
-    conversions = {"3" => "Pling", "5" => "Plang", 7 => "Plong"}
-    primes.each { }
+    return num.to_s if primes.empty?
+    conversions = {3 => "Pling", 5 => "Plang", 7 => "Plong"}
+    s = []
+    primes.each do |prime|
+      s << conversions[prime]
+    end
+    s.join
   end
 
-  def get_primes(num)
+  def self.get_primes(num)
     primes = []
-    [3,5,7].each {|prime| prime << primes if num % prime == 0}
-
+    [3,5,7].each {|prime| primes << prime if num.to_i % prime == 0}
+    primes
+  end
 end
